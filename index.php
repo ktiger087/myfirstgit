8e4332b28c02273d536b3e93fdf1ba4333b5f74e
Korin Tiger <Korin.Tiger@tzel.co.il>
2018-02-07 10:49:08 +0200

    version one

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+
+        <form action="action.php">
+
+            <input type="text" name="user" placeholder="Username">
+            <br><br>
+            <input type="pwd" name="pwd" placeholder="Password">
+            <br><br>
+            <input type="submit" value="Submit">
+        </form> 
+
+    </body>
+</html>
